3990bdb8791a887ac7e7e8aad146d30805f18861
Michael Tarimo, MD, Dev <56628755+Michaeltarimo@users.noreply.github.com>
2024-10-20 17:03:16 +0300

    push changes

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,12 +1,5 @@
-"use client";
-import React from 'react';
-import DashboardContent from '@/components/ui/dashboard';
+import DashboardContent from "@/components/ui/dashboard";
 
 export default function Dashboard() {
-  return (
-    <div className="p-6 bg-gray-100 dark:bg-neutral-900 min-h-screen">
-      <h1 className="text-3xl font-bold text-gray-900 dark:text-white mb-8">Dashboard</h1>
-      <DashboardContent />
-    </div>
-  );
+  return <DashboardContent />;
 }
