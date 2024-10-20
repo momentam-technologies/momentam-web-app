25145c90e695a9e159a823ff4be56d69bc71fde4
Michael Tarimo, MD, Dev <56628755+Michaeltarimo@users.noreply.github.com>
2024-10-20 17:49:19 +0300

    push changes again

--- src/app/dashboard/messages/page.js
+++ src/app/dashboard/messages/page.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect, Suspense } from 'react';
+import React, { useState, useEffect, Suspense, useRef } from 'react';
 import { IconSend, IconLoader, IconUser, IconPaperclip, IconMoodSmile } from '@tabler/icons-react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { useSearchParams } from 'next/navigation';
