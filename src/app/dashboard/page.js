cab5efde5fc6960ae474eb28146b4b6692250ce0
Michael Tarimo, MD, Dev <56628755+Michaeltarimo@users.noreply.github.com>
2024-10-20 18:11:11 +0300

    push another error fix

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -9,9 +9,6 @@ const DynamicDashboardContent = dynamic(
 
 export default function Dashboard() {
   return (
-    <div className="p-6 bg-gray-100 dark:bg-neutral-900 min-h-screen">
-      <h1 className="text-3xl font-bold text-gray-900 dark:text-white mb-8">Dashboard</h1>
       <DynamicDashboardContent />
-    </div>
   );
 }
